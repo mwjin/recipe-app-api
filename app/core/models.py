1ff5476d17cac64e60e27a705c95874a4d47ee44
Minwoo Jeong <jeongmwj@gmail.com>
2022-03-22 12:34:13 +0900

    Update the user model
    - Add `name` field
    - Remove `is_staff` property and add `is_staff` field

--- app/core/models.py
+++ app/core/models.py
@@ -9,13 +9,13 @@ from django.db import models
 class MyUserManager(BaseUserManager):
     """Manager for my custom user model"""
 
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates a new user"""
         if not email:
             raise ValueError("User must have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -25,6 +25,7 @@ class MyUserManager(BaseUserManager):
         """Create and save a new superuser with given details"""
         user = self.create_user(email, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
@@ -34,7 +35,9 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
 
     email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = MyUserManager()
 
@@ -43,7 +46,3 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Returning string representation of our user"""
         return self.email
-
-    @property
-    def is_staff(self):
-        return self.is_superuser
