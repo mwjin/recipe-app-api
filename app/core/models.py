d5835c1c02c66092cefd0cd7dbb6a24c8cb1c885
Minwoo Jeong <jeongmwj@gmail.com>
2022-04-06 22:39:08 +0900

    Use `settings.AUTH_USER_MODEL`
    in order to avoid hard coding when we replace the user model with another

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -51,7 +52,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Tag(models.Model):
     """Tag to be used for a recipe"""
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
     def __str__(self):
